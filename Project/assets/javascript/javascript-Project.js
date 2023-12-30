618420c7d65a145a6a936fa6fda62214b1d2c15d
Lucas Lalumière <lucas.lalum@gmail.com>
2023-12-30 02:15:47 -0500

    Finished Project.
    Finished comments and cleanup

--- Project/assets/javascript/javascript-Project.js
+++ Project/assets/javascript/javascript-Project.js
@@ -1,4 +1,21 @@
+/*
 
+__/\\\\\\\\\\\\\______/\\\\\\\\\___________/\\\\\___________/\\\\\\\\\\\__/\\\\\\\\\\\\\\\________/\\\\\\\\\__/\\\\\\\\\\\\\\\_        
+ _\/\\\/////////\\\__/\\\///////\\\_______/\\\///\\\________\/////\\\///__\/\\\///////////______/\\\////////__\///////\\\/////__       
+  _\/\\\_______\/\\\_\/\\\_____\/\\\_____/\\\/__\///\\\__________\/\\\_____\/\\\_______________/\\\/_________________\/\\\_______      
+   _\/\\\\\\\\\\\\\/__\/\\\\\\\\\\\/_____/\\\______\//\\\_________\/\\\_____\/\\\\\\\\\\\______/\\\___________________\/\\\_______     
+    _\/\\\/////////____\/\\\//////\\\____\/\\\_______\/\\\_________\/\\\_____\/\\\///////______\/\\\___________________\/\\\_______    
+     _\/\\\_____________\/\\\____\//\\\___\//\\\______/\\\__________\/\\\_____\/\\\_____________\//\\\__________________\/\\\_______   
+      _\/\\\_____________\/\\\_____\//\\\___\///\\\__/\\\_____/\\\___\/\\\_____\/\\\______________\///\\\________________\/\\\_______  
+       _\/\\\_____________\/\\\______\//\\\____\///\\\\\/_____\//\\\\\\\\\______\/\\\\\\\\\\\\\\\____\////\\\\\\\\\_______\/\\\_______ 
+        _\///______________\///________\///_______\/////________\/////////_______\///////////////________\/////////________\///________
+
+
+-----------------------------.
+ * Student: Lucas Lalumiere  |
+ * ID: 2278139               |
+ * BIXI PROJECT - JS         |
+ *--------------------------*/
 
 "use strict"
 
@@ -8,10 +25,10 @@ let tripResultDiv = document.getElementById('trip_input_results');
 let userResultDiv = document.getElementById('user_input_results');
 let durationSlider = document.getElementById('slider_container');
 let stationContainer = document.querySelector('.station_containers');
-let nameInputElement = document.querySelector('#nameInput');
+let nameInputElement = document.querySelector('#name_Input');
 let additionalInfoDiv = document.getElementById('station_aditional_info');
-let emailInputElement = document.querySelector('#emailInput');
-let stationInputElement = document.querySelector('#stationInput');
+let emailInputElement = document.querySelector('#email_Input');
+let stationInputElement = document.querySelector('#station_Input');
 
 //API variables
 const API_URL = "http://129.80.194.57";
@@ -25,34 +42,47 @@ const STATION_DISTANCE_URL = `${API_URL}/distance`;
 
 
 
+//-----------------------------------------------------------------------------------
+// - Function that processes user input (FirstName,Email,StationId)
+//   and retreives user, member and station data using fetch.
+// - Displays the results by populating HTML elements .innerHTML attributes.
+//-----------------------------------------------------------------------------------
 async function DisplaySearchResults() {
 
-
+    // Clear the results
     userResultDiv.innerHTML = "";
 
+    // retreive User input values
     let firstName = nameInputElement.value;
     let email = emailInputElement.value;
     let stationID = stationInputElement.value;
 
+    //If user leaves a field empty, stop the function.
     // if (firstName === "" || email === "" || stationID === "")
     // {
     //     alert("Please fill in all fields");
     //     return
     // }
 
-
+    // Use API to fetch user info from the users input values and assign response to variable.
     // let userData = await fetchAPIData(`${USER_INFO_URL}/${firstName}/${email}`);
     let userData = await fetchAPIData(`${USER_INFO_URL}/Zach/aang@avatar.com`);
     console.log(userData);
+
+    //If the fetch returns an empty array, no users were found:
     if (userData.length === 0) {
-        userResultDiv.innerHTML = '<h1>No user match.</h1>'
+        userResultDiv.innerHTML = '<h1>No user match.</h1>' // display to user.
         return
     }
 
+    // Retreive the user's membershipTypeID from the API response
     let membershipID = userData[0].MembershipTypeId
+
+    // Use API to fetch membership details of the user.
     let membershipData = await fetchAPIData(`${MEMBER_INFO_URL}/${membershipID}`);
     console.log(membershipData);
 
+    //Fill the Result HTML element with the results of the fetch.
     userResultDiv.innerHTML += `
     <div id="membership_container">
         <label>Membership Type</label>
@@ -66,10 +96,11 @@ async function DisplaySearchResults() {
     </div>
     `;
 
-     
+    // Fetch station data based on the user's starting station input
     let stationData = await fetchAPIData(`${STATION_INFO_URL}/${stationID}`);
     console.log(stationData)
 
+    // Display the starting station information in the user result div
     userResultDiv.innerHTML += `
     <div id="station_container">
         <label>Starting Station</label>
@@ -91,6 +122,7 @@ async function DisplaySearchResults() {
     </div>
     `;
 
+    // un-hide the next input prompt 'trip_input_container' and hide the current.
     document.getElementById('trip_input_container').style.display = 'flex';
     document.getElementById('user_input_container').style.display = 'none';
 }
@@ -98,8 +130,12 @@ async function DisplaySearchResults() {
 
 
 
-
-
+//-----------------------------------------------------------------------------------
+// - Similar to the former, this method uses user input (slider value)
+//   to find nearby stations based on the Maximum minutes of their trip.
+// - Displays these nearby stations in the HTML, each having buttons that 
+//   a user can use to select their desired destination/station.
+//-----------------------------------------------------------------------------------
 async function DisplayPossibleTrips() {
 
     // Assign the current slider value to variable
@@ -124,10 +160,9 @@ async function DisplayPossibleTrips() {
     let nearbyStationsData = await fetchAPIData(`${NEARBY_STATIONS_URL}/${StartingStationID}`)
     console.log(nearbyStationsData)
 
-    // Filter out all stations who arent within possible range.
+    // Filter out all stations who arent within possible range. If the filtered array is empty, escape function and display it to user
     const filteredStations = nearbyStationsData.filter(station => station.Distance <= maxPossibleDistance);
-    if (filteredStations.length === 0)
-    {
+    if (filteredStations.length === 0) {
         tripResultDiv.innerHTML = '<h1>No trip results.</h1>'
         return
     }
@@ -143,11 +178,14 @@ async function DisplayPossibleTrips() {
     // display each(filtered) nearby station as a HTML container:
     filteredStations.forEach(async station => {
 
+        // Use fetch to retreive the information of each station.
         let stationData = await fetchAPIData(`${STATION_INFO_URL}/${station.SecondStationId}`)
 
         //Calculate the amount of AVAILABLE Docks 
         let availableDocks = (stationData[0]['Total Docks']) - (stationData[0].RegularBikesCount + stationData[0].ElectricBikesCount)
 
+        // Pupulate the html with container consisting of the station info (Name,ID,AvailableDocks). 
+        // Each container has a button that selects the station as the user's endStation and finds the path to that station.
         tripResultDiv.innerHTML += `
         <div class="station_containers">
             <h2>${stationData[0].StationName}</h2>
@@ -165,46 +203,55 @@ async function DisplayPossibleTrips() {
     });
 }
 
-
-
+//-----------------------------------------------------------------------------------
+// - Once the user has chosen their end station, this method 
+//   displays the path of stations starting from their starting station.
+// - Progressively displays the upcoming stations with delays relative to their
+//   distance from eachother.
+// - Once a station has been loaded to the HTML, the user has the option to "select"
+//   a station to display further information about said station.
+//-----------------------------------------------------------------------------------
 async function DisplayTripPath(destinationId) {
 
-
+    //Refresh the HTML and show a header and instructions
     tripResultDiv.innerHTML = `<h3>Path to destination</h3>
                                <label>(Click for more information below)</label>`;
 
 
-    let startStationId = stationInputElement.value;
-    let endStationId = destinationId;
+    //Assign start and end stations to variables
+    let startStationId = stationInputElement.value; // initial user input
+    let endStationId = destinationId;               // user's selected end station
 
+    // retreive the station path from fetching the API using the ID's of the start and end stations.
     let pathData = await fetchAPIData(`${STATION_PATH_URL}/${startStationId}/${endStationId}`);
 
     console.log(pathData);
 
+    // retreive the array of stations that make up the path.
     let stations = pathData[0]; // Assuming the stations are in the first element of pathData
 
-    // using for-loop because forEach conflicts with async.
+    // Loop through each station in the path array (using for-loop because forEach conflicts with async.)
     for (let i = 0; i < stations.length; i++) {
+
+        // assign to variables station ID's of the current and next array index (used to calculate delay)
         const currentStation = stations[i];
         const nextStation = stations[i + 1];
 
-        // console.log(currentStation)
-        // console.log(nextStation)
-
+        //ppopulate result container with the current station iteration (as a button)
         tripResultDiv.innerHTML += `
         <div class="station_path_buttons">
             <button class="additional_info" data-query="${currentStation.StationId}" onclick="DisplayAdditionalInfo(this.dataset.query)">Station ${currentStation.StationId}</button>
         </div>
     `;
 
-
+        // if the user is NOT the end Station:
         if (i >= 0 && i < stations.length - 1) {
+
+            //fetch the distance between the current station and the next from the API
             let distanceData = await fetchAPIData(`${STATION_DISTANCE_URL}/${currentStation.StationId}/${nextStation.StationId}`);
             let connectionDistance = distanceData[0].Distance;
-            console.log(distanceData);
-            console.log(connectionDistance)
 
-            // Delay before displaying the next station
+            // start a delay based on the distance (100 meters = 1 second) 
             await delay(connectionDistance * 10);
         }
     }
@@ -214,6 +261,15 @@ async function DisplayTripPath(destinationId) {
     tripResultDiv.innerHTML += `<h3>Arrived at destination station ${destinationStation.StationId}!</h3>`;
 }
 
+
+
+
+
+
+//----------------------------------------------------------
+// - Function that creates useless promises.
+// - Uses setTimeout to create a delay with passed in value.
+//-----------------------------------------------------------
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -221,42 +277,49 @@ function delay(ms) {
 
 
 
-// async function fetchAPIData(url) {
-//     const response = await fetch(url);
-//     if (response.ok) {
-//         const ApiData = await response.json(); // Await the result before using it
-//         return ApiData;
-//     } else {
-//         throw new Error(`Failed to fetch information from API: ${response.status}`);
-//     }
-// }
-
-
+//--------------------------------------------------------
+// - Asynchronous function to fetch data from the specified URL.
+// ---------------------------------------------------------
 async function fetchAPIData(url) {
     try {
+        // Attempt to fetch data from the passed URL.
         const response = await fetch(url);
+
+        // Check if response status is OK...
         if (response.ok) {
+            // If so, parse and return the JSON data from the response
             return await response.json();
         } else {
+            // If not, throw an error with message
             throw new Error(`Failed to fetch information from API: ${response.status}`);
         }
     } catch (error) {
+        // Catch and log errors that happend during fetching.
         console.error(error.message);
     }
 }
 
-
+//------------------------------------------------------------------
+// - Method used to dynamically display the sliders value in the html.
+// - Called from the html.
+//------------------------------------------------------------------
 function updateSliderValue(value) {
+    // 1. assign element to variable resposible for displaying the sliders value
     const sliderValueElement = document.getElementById('slider_value');
+    // 2. update the html text based on the passed in value.
     sliderValueElement.innerText = value + " min";
 }
 
-
+//--------------------------------------------------------------------
+// - Function that upon the click of a path station, will display
+//   additional station data of said station under the path of stations.
+// ------------------------------------------------------------------------
 async function DisplayAdditionalInfo(stationId) {
 
-
+    // Fetch aditional about the selected station using fetch 
     let stationData = await fetchAPIData(`${STATION_INFO_URL}/${stationId}`);
 
+    // Update the html content of the additionalInfoDiv with station details(Longitude,Latitude & Available Docks).
     additionalInfoDiv.innerHTML = `
     <div id="additional_info_container">
         <label>Selected Station</label>
